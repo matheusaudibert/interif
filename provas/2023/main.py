fca206f1424c6e704b42a0ce2ab34fea02c86f4c
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-05-08 15:30:19 -0300

    feat(Exercicio): Problema A da prova de 2025: "O Bruxo e os Frascos"

---
+++ provas/2023/main.py
@@ -0,0 +1,17 @@
+def conta_vogal(texto):
+  vogais = 'aeiouAEIOU'
+  contagem = 0;
+  
+  for letra in texto:
+    if letra in vogais:
+      contagem += 1
+    
+  return contagem % 3
+
+     
+nome= input("Digite o nome: ")
+vogais = conta_vogal(nome)
+print(f"frasco {vogais}")
+
+  
+  
