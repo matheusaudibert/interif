9993a111c6a2d27a8ff689f0a9540f0e278f7740
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-06-26 16:18:54 -0300

    Problema B - 2018 Primeira Fase

---
+++ provas/2018/primeira_fase/problema_b.py
@@ -0,0 +1,16 @@
+# Problema B 
+# Jogo de Truco
+
+# Exemplos de Entradas - Exemplos de Saídas 
+# 1 - 2 3 
+# 11 - 12 13 1 2 3 
+# 13 - 1 2 3 
+# 3 - 0 
+
+cartas = [4, 5, 6, 7, 11, 12, 13, 1, 2, 3]
+carta = int(input())
+maiores = [c for c in cartas if cartas.index(c) > cartas.index(carta)]
+if maiores:
+    print(*maiores)
+else:
+    print(0)
