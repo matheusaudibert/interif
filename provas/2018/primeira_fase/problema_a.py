5e12cc4e4ae07ab027a3ef99e5245addb7395bef
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-06-26 15:49:33 -0300

    Problema A - 2018 Primeira Fase

--- provas/2018/primeira_fase/problema_a.py
+++ provas/2018/primeira_fase/problema_a.py
@@ -1,7 +1,16 @@
 # Problema A
 # Funções Matemáticas
 
+# Exemplos de Entradas  Exemplos de Saídas 
+# 5 3  - Beto ganhou 
+# 2 30 - Carlos ganhou 
+# 2 100 - Carlos ganhou 
+# 30 20 - Beto ganhou  
+# 15 5 - Rafael Ganhou 
+
+
 x,y=map(int,input().split())
 valores={'Rafael':(3*x)**2+y**2,'Beto':2*x**2+(5*y)**2,'Carlos':-100*x+y**3}
 ganhador=max(valores,key=valores.get)
 print(f'{ganhador} ganhou')
+
