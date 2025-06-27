2a2d9c9d983c9765f5d2c1f47742882c3eb20b74
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-06-26 23:47:53 -0300

    Problema D - 2018 Primeira Fase

--- provas/2018/primeira_fase/problema_b.py
+++ provas/2018/primeira_fase/problema_b.py
@@ -2,15 +2,23 @@
 # Jogo de Truco
 
 # Exemplos de Entradas - Exemplos de Saídas 
-# 1 - 2 3 
-# 11 - 12 13 1 2 3 
-# 13 - 1 2 3 
-# 3 - 0 
+# 1   2 3 
+# 11  12 13 1 2 3 
+# 13  1 2 3 
+# 3   0 
 
+# Define a ordem de força das cartas, da mais fraca para a mais forte.
+# A força é determinada pela posição (índice) na lista.
 cartas = [4, 5, 6, 7, 11, 12, 13, 1, 2, 3]
+# Lê a carta do usuário.
 carta = int(input())
+# Cria uma lista com as cartas que são mais fortes que a carta do usuário.
+# A comparação é feita com base no índice da carta na lista 'cartas': um índice maior significa uma carta mais forte.
 maiores = [c for c in cartas if cartas.index(c) > cartas.index(carta)]
+# Se a lista 'maiores' não estiver vazia, imprime seus elementos.
 if maiores:
+    # O '*' desempacota a lista para imprimir os elementos separados por espaço.
     print(*maiores)
+# Caso contrário, se não houver cartas maiores, imprime 0.
 else:
     print(0)
---
+++ provas/2018/primeira_fase/problema_d.py
@@ -0,0 +1,42 @@
+# Problema D 
+# Fábrica de Automóveis
+
+# Exemplos de Entradas - Exemplos de Saídas 
+# 3 2      5100 6700
+# 4 5 7    1700 2200
+# 1 2 2
+# 300 400  
+# 500 600
+# 200 300
+
+# 4 3          21200 25800 30400
+# 10 11 12 13  28400 34600 40800
+# 14 15 16 17
+# 300 400 500
+# 400 500 600
+# 500 600 700
+# 600 700 800
+
+# Lê o número de modelos e meses
+modelos, meses = map(int, input().split())
+# Lê a quantidade de parafusos por modelo
+parafusos_por_modelo = list(map(int, input().split()))
+# Lê a quantidade de travas por modelo
+travas_por_modelo = list(map(int, input().split()))
+# Lê a produção de cada modelo em cada mês
+producao = []
+for _ in range(modelos):
+    linha_producao = list(map(int, input().split()))
+    producao.append(linha_producao)
+# Inicializa os totais por mês com 0
+total_parafusos = [0] * meses
+total_travas = [0] * meses
+# Calcula os totais por mês
+for modelo in range(modelos):
+    for mes in range(meses):
+        quantidade = producao[modelo][mes]
+        total_parafusos[mes] += parafusos_por_modelo[modelo] * quantidade
+        total_travas[mes] += travas_por_modelo[modelo] * quantidade
+# Exibe os resultados separados por tabulação
+print('\t'.join(map(str, total_parafusos)))
+print('\t'.join(map(str, total_travas)))
