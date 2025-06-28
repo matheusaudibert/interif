16a2b96b9e6414208fd1cb3122dd10811e318bff
Matheus Audibert <matheusaudibert2019@outlook.com>
2025-06-28 14:47:59 -0300

    fix(names): Fixed some code structs

--- provas/2018/primeira_fase/D.py
+++ provas/2018/primeira_fase/D.py
@@ -17,26 +17,19 @@
 # 500 600 700
 # 600 700 800
 
-# Lê o número de modelos e meses
 modelos, meses = map(int, input().split())
-# Lê a quantidade de parafusos por modelo
 parafusos_por_modelo = list(map(int, input().split()))
-# Lê a quantidade de travas por modelo
 travas_por_modelo = list(map(int, input().split()))
-# Lê a produção de cada modelo em cada mês
 producao = []
 for _ in range(modelos):
     linha_producao = list(map(int, input().split()))
     producao.append(linha_producao)
-# Inicializa os totais por mês com 0
 total_parafusos = [0] * meses
 total_travas = [0] * meses
-# Calcula os totais por mês
 for modelo in range(modelos):
     for mes in range(meses):
         quantidade = producao[modelo][mes]
         total_parafusos[mes] += parafusos_por_modelo[modelo] * quantidade
         total_travas[mes] += travas_por_modelo[modelo] * quantidade
-# Exibe os resultados separados por tabulação
 print('\t'.join(map(str, total_parafusos)))
 print('\t'.join(map(str, total_travas)))
--- provas/2023/primera_fase/A.py
+++ provas/2023/primera_fase/A.py
@@ -1,17 +1,23 @@
 # Problema A 
 # O Bruxo e os Frascos
 
+# Exemplos de Entradas  Exemplos de Saídas
+# Manticora             # frasco 1 
+  
+# Grifo                 # frasco 2 
+  
+# Dragao Fofao          # frasco 0 
+  
+# Liche bondoso bacanA  # frasco 2
+
 def conta_vogal(texto):
   vogais = 'aeiouAEIOU'
   contagem = 0
-  
   for letra in texto:
     if letra in vogais:
       contagem += 1
-    
   return contagem % 3
-     
-nome= input("Digite o nome: ")
+nome = input()
 vogais = conta_vogal(nome)
 print(f"frasco {vogais}")
 
